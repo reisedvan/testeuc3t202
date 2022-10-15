4b00a7c8dbf9157d5352b476af7f1e83dc77a9b3
MKillua <MKillua@DESKTOP-KOG2PG9>
2022-10-14 21:56:16 -0300

    Criação do Projeto

---
+++ TesteEmAula/src/testeemaula/TesteEmAula.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testeemaula;
+
+/**
+ *
+ * @author MKillua
+ */
+public class TesteEmAula {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       /*{REMOTO}
+            |
+         {LOCAL}
+            |
+        {NETBEANS}
+    
+         */
+    }
+
+}
