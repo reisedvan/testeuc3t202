988af5d12582649067b3af0f3778b9b0e7510ae6
b2mledvanreis <edvan.reis@b2ml.com.br>
2022-10-14 22:00:32 -0300

    Remoção dos comentários

--- TesteEmAula/src/testeemaula/TesteEmAula.java
+++ TesteEmAula/src/testeemaula/TesteEmAula.java
@@ -1,19 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package testeemaula;
 
-/**
- *
- * @author MKillua
- */
 public class TesteEmAula {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("Exemplo X");
     }
