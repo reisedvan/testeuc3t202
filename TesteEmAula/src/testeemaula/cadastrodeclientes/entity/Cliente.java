0687177810c69763324b57d5ff9995e235c1fcb6
MKillua <MKillua@DESKTOP-KOG2PG9>
2022-10-17 20:31:33 -0200

    Criação do pacote cadastrodeclientes e classe cliente

---
+++ TesteEmAula/src/testeemaula/cadastrodeclientes/entity/Cliente.java
@@ -0,0 +1,44 @@
+package testeemaula.cadastrodeclientes.entity;
+
+import java.sql.Date;
+
+public class Cliente {
+
+    private String cpf;
+    private String nome;
+    private String sobrenome;
+    private Date data_nascimento;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public Date getData_nascimento() {
+        return data_nascimento;
+    }
+
+    public void setData_nascimento(Date data_nascimento) {
+        this.data_nascimento = data_nascimento;
+    }
+
+}
